6bf875244a2d096d77ffad364e256d863f7376bf
Kevin Yang <kevinyang.cyen@gmail.com>
2020-10-14 17:38:28 -0700

    changed database to use queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,7 +1,14 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
 
 /// Users
+const pool = new Pool({
+  user: 'yangster',
+  password: '123',
+  host: 'localhost',
+  database: 'yangster'
+});
 
 /**
  * Get a single user from the database given their email.
@@ -67,11 +74,11 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  return pool.query(`
+  SELECT * FROM properties
+  LIMIT $1
+  `, [limit])
+  .then(res => res.rows);
 }
 exports.getAllProperties = getAllProperties;
 
